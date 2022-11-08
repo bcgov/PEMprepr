ebf5e4d043bf5f5d17b6d5e83334705c71541d71
MacKenzie <Will.MacKenzie@gov.bc.ca>
2022-11-08 14:36:16 -0800

    two new functions for align and crop of template raster

---
+++ R/create_template.R
@@ -0,0 +1,44 @@
+#' Create a raster template from AOI
+#'
+#' Sets the baseline raster template to align and standardize all raster predictor layers
+#' This package assumes to data is in a metric equal area projection most likely BCAlbers
+#'
+#' This is an essential first step.  As subsequent co-variate layers will be generated at multiple resolutions (e.g. 5, 10, 25m^2) and then disaggregate'd back to the lowest resolution.
+#' Having the aoi set 100m break-points facilitates this.
+#'
+#' @param aoi is a sf object bounding box created expanded in aoi_snap function(e.g. polygon).
+#' @param method Options are _shrink_ or _expand_. _Shrink_ will snap the aoi in to the nearest 100m. _Expand_ will snap the aoi out to the nearest 100m.
+#' @return a terra raster
+#' @keywords aoi, raster, template, crop, align
+#' @export
+#' @examples
+
+# read in the aoi
+res = 10
+aoi <- terra::vect( "./temp_data/aoi.gpkg")
+output =
+
+create_template <- function(aoi, res, outpath = ""){
+resfolder = "10m"
+    template <- terra::rast(aoi, resolution = c(res,res))
+  values(template) <- 1:ncell(template)
+#plot(template)
+terra::writeRaster(template, file.path(paste(outpath, resfolder, "template.tif", sep = "/")), overwrite = TRUE)
+return(TRUE)
+}
+
+
+dem <- terra::rast( "./temp_data/10_DTM_mosaic.TIF")
+rtemplate <- terra::rast( "./temp_data/template.tif")
+
+align_raster <- function(covariate, resfolder, rtemplate = "", outpath = ""){
+  #resfolder = "10m"
+  covariate <- terra::rast(covariate, resolution = c(res,res))
+  rtemplate <- terra::rast(rtemplate)
+  crs(covariate) <- crs(rtemplate)
+  covariate <- terra::crop(dem, rtemplate) %>%
+    terra::resample(dem, rtemplate)
+  #plot(template)
+  terra::writeRaster(covariate, file.path(paste(outpath, resfolder, covariate_name, ".tif", sep = "/")), overwrite = TRUE)
+  return(TRUE)
+}
