54b4dfe1bb79fc5843ba789a18b8ab5f5b546de6
Colin Chisholm <colin.chisholm@unbc.ca>
2022-11-08 21:40:43 +0000

    not complete at all

--- R/raster_align.R
+++ R/raster_align.R
@@ -1,14 +1,14 @@
-#' Raster Align
+#' @title Raster Align
 #'
-#' Ensures raster aligns with vector data.
+#' @description  Ensures raster aligns with vector data.
 #'
-#' For example, does the DEM align with the output of `aoi_snap`
+#' @details For example, does the DEM align with the output of `aoi_snap`
 #'
-#' @param raster a
-#' @param abc
+#' @param raster single channel terra::rast object
+#' @param polygon the aoi polygon
 #'
-#' @return
-#' # ATexport
+#' # `@`return something
+#' # `@`export  uncomment this to make it usable
 
 
 raster_align <- function(raster, polygon) {
