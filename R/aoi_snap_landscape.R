e16268bc3f5eee49b080885e48c6a4e44a3db983
gcperk <genevieve.c.perkins@gmail.com>
2022-11-09 12:52:34 -0800

    streamline aoi_function

--- R/aoi_snap_landscape.R
+++ R/aoi_snap_landscape.R
@@ -3,35 +3,25 @@
 
 #library(sf)
 library(bcdata)
+import(magrittr)
+
 #library(dplyr)
 #library(foreach)
 
 # testing
-in_aoi <- sf::st_read(file.path('temp', "aoi.gpkg"))
+#in_aoi <- sf::st_read(file.path('temp', "aoi.gpkg"))
 #out_path = shape_raw_dir <- file.path('temp')
 
 
-aoi_snap_landscape <- function(in_aoi, out_path = shape_raw_dir){
+aoi_snap_landscape <- function(in_aoi){
 
-  # Uses date filter which filters cutblock ages less than 20 years, or 7305 days
   watershed <- bcdc_query_geodata("3ee497c4-57d7-47f8-b030-2e0c03f8462a") %>%
     bcdata::filter(INTERSECTS(in_aoi)) %>%
     collect()
 
-  bb <- sf::st_bbox(watershed)
+  box <- PEMprepr::aoi_snap(watershed, "extend")
 
-  xmin <- floor(bb$xmin / 100)*100
-  xmax <- ceiling(bb["xmax"] / 100) * 100
-  ymin <- floor(bb$ymin / 100)*100
-  ymax <- ceiling(bb["ymax"] / 100) * 100
-
-  box <- matrix(c(xmin, ymin, xmin, ymax, xmax, ymax, xmax, ymin, xmin, ymin), ncol = 2, byrow = TRUE)
-  box <- sf::st_polygon(list(box))
-  box <- sf::st_sfc(box, crs=sf::st_crs(in_aoi))
-  box <- sf::st_as_sf(box)
-
-  print("Extent is:")
-  print(sf::st_bbox(box))
+ # mapview::mapview(list(box, in_aoi, watershed))
 
   return(box)
 
