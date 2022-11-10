416cda0aa388d9b3a2c4c6f187f2c4a811176ae8
Colin Chisholm <colin.chisholm@unbc.ca>
2022-11-10 19:37:17 +0000

    working up to TEM

--- R/create_base_vectors.R
+++ R/create_base_vectors.R
@@ -1,43 +1,8 @@
-
-# ## Overview
-#
-# This script downloads the relevant spatial data for BEC zones, VRI, TEM, waterbodies and the road network used in stage 1 of PEM processing. Data is downloaded directly from the [BC Data Catalogue](https://catalogue.data.gov.bc.ca/dataset?download_audience=Public) using the [bcdata](https://github.com/bcgov/bcdata) package.
-#
-# ### Datasets to download
-#
-# * BEC - [Biogeoclimatic Ecosystems Classification](https://catalogue.data.gov.bc.ca/dataset/bec-map) are used to define and/or select specific "subzones" within a defined study area. Note this can be also pointed to different file where more updated version is available.
-#
-# * Vegetation Resource Inventory (VRI) - This layer includes a variety of vegetation measure, including cutblock age, TEM data and ..... Detailed data standards can be found [here](https://www2.gov.bc.ca/gov/content/industry/forestry/managing-our-forest-resources/forest-inventory/data-management-and-access/vri-data-standards).
-#
-# * Freshwater Atlas -  The atlas is separated into the different types of waterbodies (lakes, rivers, wetlands, streams, man made, etc.), which requires a seperate download per type. Alternatively a single combined layer can be downloaded by is limited to linear data type. A parameter within the function can be set to "polygon" (the default option) or "linear".
-#
-# * Road network THIS STILL NEEEDS WORK
-#
-# * Road network - In previous works, the [raw road network](https://catalogue.data.gov.bc.ca/dataset/digital-road-atlas-dra-master-partially-attributed-roads) was found to be too detailed. The raw road network is filtered to only keep named roads, and then the up to date [Forest Service Road (FSR) layer](https://catalogue.data.gov.bc.ca/dataset/forest-tenure-road-segment-lines) is downloaded and merged with the filtered road network to produce a really good representation of where roads are actually located on the landscape.
-#
-# * Fire - Fire and fire intensity are used used to assist in identifying areas where a high cost is applied to reduce sampling. This includes:
-# -[current](https://catalogue.data.gov.bc.ca/dataset/fire-perimeters-current)
-# -[historical](https://catalogue.data.gov.bc.ca/dataset/fire-perimeters-historical) polygons.
-# -[severity](https://catalogue.data.gov.bc.ca/dataset/c58a54e5-76b7-4921-94a7-b5998484e697).
-#
-# * Accessing the [consolidated cutblock layer](https://catalogue.data.gov.bc.ca/dataset/harvested-areas-of-bc-consolidated-cutblocks-) using the bcdata package. The FTen layer is also used to identify very recent cutblocks ()
-#
-# Note the detailed
-# Tenure (private ownership) 	WHSE_CADASTRE.PMBC_PARCEL_FABRIC_POLY_FA_SVW	BCGW
-# Detailed roads layers. In some cases regionally specific roads are available through other collaborative projects (cummulative effects). These are not firectly available through the BCGW.
-
-
-# inputs: aoi_snapped (shapefile)
-# output filenames:
-
-
-
-
-
-library(sf)
-library(bcdata)
-library(dplyr)
-library(foreach)
+## Original GP notes at bottome
+## ISSUES
+## - TEM download not working
+## - options call ... change to global env.
+## - remove foreach loop in water
 
 
 
@@ -73,18 +38,19 @@ library(foreach)
 #' @param out_path character string that points the output location
 #'
 #' @export
-#'
+#' @import sf
+#' @import bcdata
 #' @examples
 #' create_base_vectors <- function(in_aoi = your_polygon, out_path = "00_raw_inputs/vector")
 #'
 create_base_vectors <- function(in_aoi, out_path = "00_raw_inputs/vector"){
 
 
-  # testing
-  #in_aoi <- st_read(file.path('temp', "aoi.gpkg"))
-  #out_path = shape_raw_dir <- file.path('temp')
-
-
+  # # testing
+  # #in_aoi <- st_read(file.path('temp', "aoi.gpkg"))
+  # #out_path = shape_raw_dir <- file.path('temp')
+  #
+  #
   if(missing(in_aoi)) stop("'aoi' is missing with no default")
 
   # Second, detect object type and convert where necessary
@@ -118,111 +84,113 @@ create_base_vectors <- function(in_aoi, out_path = "00_raw_inputs/vector"){
 
   # Adjust max download size based on AOI
   options(bcdata.max_geom_pred_size = as.numeric(st_area(in_aoi)) + 10)
+  #
+  #
+  # # 1) BEC Biogeographical linework
+  # bec <- bcdc_query_geodata("f358a53b-ffde-4830-a325-a5a03ff672c3") %>%
+  #   bcdata::filter(INTERSECTS(in_aoi)) %>%
+  #   collect() %>%
+  #   {if(nrow(.) > 0) st_intersection(., in_aoi) else .}
+  #
+  # st_write(bec, file.path(out_path, "bec.gpkg"), delete_dsn = TRUE,
+  #          delete_layer = TRUE)
+  #
+  #
+  # # 2) Download VRI
+  # vri <- bcdc_query_geodata("2ebb35d8-c82f-4a17-9c96-612ac3532d55") %>%
+  #   bcdata::filter(INTERSECTS(in_aoi)) %>%
+  #   bcdata::select(c("BCLCS_LEVEL_2","BCLCS_LEVEL_4","PROJ_AGE_CLASS_CD_1", "SPECIES_CD_1")) %>% # Treed sites
+  #   collect() %>%
+  #   {if(nrow(.) > 0) st_intersection(., in_aoi) else .}
+  #
+  # st_write(vri, file.path(out_path, "vri.gpkg"), delete_dsn = TRUE,
+  #          delete_layer = TRUE)
+  #
+  #
+  # # post process VRI data into classes:
+  # # Depending on the study area we want to focus on sampling in older areas - class 4 (60-80) or 5 (80 + )
+  # # To do this we will define 2 vri age classess to exclude from the sampling area
+  # # class 1 and 2 (0 - 40 yrs)
+  # # class 1-3 (0 - 60 years)
+  #
+  # #  vri <- st_read(file.path(out_path, "vri.gpkg")) %>%
+  # #    dplyr::select(c("BCLCS_LEVEL_2","BCLCS_LEVEL_4","PROJ_AGE_CLASS_CD_1", "SPECIES_CD_1"))
+  #
+  #     vri_class2 <- vri %>%
+  #       dplyr::mutate(age_class = as.numeric(PROJ_AGE_CLASS_CD_1)) %>%
+  #       dplyr::filter(age_class < 3)
+  #     st_write(vri_class2, file.path(out_path, "vri_class1_2.gpkg"))
+  #
+  #     vri_class3 <- vri %>%
+  #       dplyr::mutate(age_class = as.numeric(PROJ_AGE_CLASS_CD_1)) %>%
+  #       dplyr::filter(age_class == 3)
+  #     st_write(vri_class3, file.path(out_path, "vri_class3.gpkg"))
+  #
+  #
+  # ######### this section is currently in test phase (related to removal fo deciduous areas in Date Creek after these were found to be highly selected and challanging to sample)
+  #
+  # # vri - deciduous leading - this needs to be appled for some
+  #
+  # # STILL TO DO - for areas with deciduous leading (AT, EP) Aspen and paper burch these should be seperated using the code "SPECIES_CD_1" == AT|EP.
+  # # ie important in Date Creek
+  #
+  # vri_decid <- vri %>%
+  #   dplyr::filter(SPECIES_CD_1 %in% c("AT","EP")) # note might need to adjust for some areas of interest
+  #
+  # st_write(vri_decid, file.path(out_path, "vri_decid.gpkg"))
+  #
+  # ######### end of test section
+  #
+  # # 3) Download recent cutblocks (within last 20 years)
+  # message("\rDownloading cutblock layers")
+  #
+  # # Uses date filter which filters cutblock ages less than 20 years, or 7305 days
+  # cutblocks <- bcdc_query_geodata("b1b647a6-f271-42e0-9cd0-89ec24bce9f7") %>%
+  #   bcdata::filter(INTERSECTS(in_aoi)) %>%
+  #   collect()
+  #
+  # cutblocks <- cutblocks %>%
+  #   {if(nrow(.) > 0) st_intersection(., in_aoi) else .} %>%
+  #   dplyr::filter(as.numeric(format(Sys.time(), "%Y")) - HARVEST_YEAR <= 20)
+  #
+  # st_write(cutblocks, file.path(out_path, "cutblocks.gpkg"), delete_dsn = TRUE,
+  #          delete_layer = TRUE)
+  #
+  # # 4) ften  - Download latest harvest layer
+  #
+  # ften <- bcdc_query_geodata("cff7b8f7-6897-444f-8c53-4bb93c7e9f8b") %>%
+  #   bcdata::filter(INTERSECTS(in_aoi)) %>%
+  #   bcdata::select("HARVEST_AUTH_STATUS_CODE", "ISSUE_DATE", "CURRENT_EXPIRY_DATE_CALC", "LIFE_CYCLE_STATUS_CODE", "FILE_STATUS_CODE") %>%# Treed sites
+  #   collect()
+  #
+  #
+  # ften <- ften %>%
+  #   filter(ISSUE_DATE >2000) # might need to adjust this to dynamic
+  #
+  # ften <- ften %>%
+  #   dplyr::select("HARVEST_AUTH_STATUS_CODE", "ISSUE_DATE", "CURRENT_EXPIRY_DATE_CALC",
+  #                 "LIFE_CYCLE_STATUS_CODE", "FILE_STATUS_CODE","FEATURE_AREA")
+  #
+  # st_write(ften, file.path(out_path, "ften.gpkg"), delete_dsn = TRUE,
+  #          delete_layer = TRUE)
+  #
+  # cutblocks_ften <- bind_rows(cutblocks, ften)
+  # cutblocks_ften <- bind_rows( ften, cutblocks)
+  # st_write(cutblocks_ften, file.path(out_path, "cutblocks_ften.gpkg"),delete_dsn = TRUE,
+  #          delete_layer = TRUE)
+  #
+  #
+  ### BREAKS HERE ...
+  # # 5) TEM
+  # message("\rDownloading TEM layers")
+  # tem <- bcdc_query_geodata("0a83163b-a62f-4ce6-a9a1-21c228b0c0a3") %>%
+  #   bcdata::filter(INTERSECTS(in_aoi)) %>%
+  #   collect() %>%
+  #   {if(nrow(.) > 0) st_intersection(., in_aoi) else .}
 
 
-  # 1) BEC Biogeographical linework
-  bec <- bcdc_query_geodata("f358a53b-ffde-4830-a325-a5a03ff672c3") %>%
-    bcdata::filter(INTERSECTS(in_aoi)) %>%
-    collect() %>%
-    {if(nrow(.) > 0) st_intersection(., in_aoi) else .}
-
-  st_write(bec, file.path(out_path, "bec.gpkg"), delete_dsn = TRUE,
-           delete_layer = TRUE)
-
-
-  # 2) Download VRI
-  vri <- bcdc_query_geodata("2ebb35d8-c82f-4a17-9c96-612ac3532d55") %>%
-    bcdata::filter(INTERSECTS(in_aoi)) %>%
-    bcdata::select(c("BCLCS_LEVEL_2","BCLCS_LEVEL_4","PROJ_AGE_CLASS_CD_1", "SPECIES_CD_1")) %>% # Treed sites
-    collect() %>%
-    {if(nrow(.) > 0) st_intersection(., in_aoi) else .}
-
-  st_write(vri, file.path(out_path, "vri.gpkg"), delete_dsn = TRUE,
-           delete_layer = TRUE)
-
-
-  # post process VRI data into classes:
-  # Depending on the study area we want to focus on sampling in older areas - class 4 (60-80) or 5 (80 + )
-  # To do this we will define 2 vri age classess to exclude from the sampling area
-  # class 1 and 2 (0 - 40 yrs)
-  # class 1-3 (0 - 60 years)
-
-  #  vri <- st_read(file.path(out_path, "vri.gpkg")) %>%
-  #    dplyr::select(c("BCLCS_LEVEL_2","BCLCS_LEVEL_4","PROJ_AGE_CLASS_CD_1", "SPECIES_CD_1"))
-
-      vri_class2 <- vri %>%
-        dplyr::mutate(age_class = as.numeric(PROJ_AGE_CLASS_CD_1)) %>%
-        dplyr::filter(age_class < 3)
-      st_write(vri_class2, file.path(out_path, "vri_class1_2.gpkg"))
-
-      vri_class3 <- vri %>%
-        dplyr::mutate(age_class = as.numeric(PROJ_AGE_CLASS_CD_1)) %>%
-        dplyr::filter(age_class == 3)
-      st_write(vri_class3, file.path(out_path, "vri_class3.gpkg"))
-
-
-  ######### this section is currently in test phase (related to removal fo deciduous areas in Date Creek after these were found to be highly selected and challanging to sample)
-
-  # vri - deciduous leading - this needs to be appled for some
-
-  # STILL TO DO - for areas with deciduous leading (AT, EP) Aspen and paper burch these should be seperated using the code "SPECIES_CD_1" == AT|EP.
-  # ie important in Date Creek
-
-  vri_decid <- vri %>%
-    dplyr::filter(SPECIES_CD_1 %in% c("AT","EP")) # note might need to adjust for some areas of interest
-
-  st_write(vri_decid, file.path(out_path, "vri_decid.gpkg"))
-
-  ######### end of test section
-
-  # 3) Download recent cutblocks (within last 20 years)
-  message("\rDownloading cutblock layers")
-
-  # Uses date filter which filters cutblock ages less than 20 years, or 7305 days
-  cutblocks <- bcdc_query_geodata("b1b647a6-f271-42e0-9cd0-89ec24bce9f7") %>%
-    bcdata::filter(INTERSECTS(in_aoi)) %>%
-    collect()
-
-  cutblocks <- cutblocks %>%
-    {if(nrow(.) > 0) st_intersection(., in_aoi) else .} %>%
-    dplyr::filter(as.numeric(format(Sys.time(), "%Y")) - HARVEST_YEAR <= 20)
-
-  st_write(cutblocks, file.path(out_path, "cutblocks.gpkg"), delete_dsn = TRUE,
-           delete_layer = TRUE)
-
-  # 4) ften  - Download latest harvest layer
-
-  ften <- bcdc_query_geodata("cff7b8f7-6897-444f-8c53-4bb93c7e9f8b") %>%
-    bcdata::filter(INTERSECTS(in_aoi)) %>%
-    bcdata::select("HARVEST_AUTH_STATUS_CODE", "ISSUE_DATE", "CURRENT_EXPIRY_DATE_CALC", "LIFE_CYCLE_STATUS_CODE", "FILE_STATUS_CODE") %>%# Treed sites
-    collect()
-
-
-  ften <- ften %>%
-    filter(ISSUE_DATE >2000) # might need to adjust this to dynamic
-
-  ften <- ften %>%
-    dplyr::select("HARVEST_AUTH_STATUS_CODE", "ISSUE_DATE", "CURRENT_EXPIRY_DATE_CALC",
-                  "LIFE_CYCLE_STATUS_CODE", "FILE_STATUS_CODE","FEATURE_AREA")
-
-  st_write(ften, file.path(out_path, "ften.gpkg"), delete_dsn = TRUE,
-           delete_layer = TRUE)
-
-  cutblocks_ften <- bind_rows(cutblocks, ften)
-  cutblocks_ften <- bind_rows( ften, cutblocks)
-  st_write(cutblocks_ften, file.path(out_path, "cutblocks_ften.gpkg"),delete_dsn = TRUE,
-           delete_layer = TRUE)
-
-  # 5) TEM
-  message("\rDownloading TEM layers")
-  tem <- bcdc_query_geodata("0a83163b-a62f-4ce6-a9a1-21c228b0c0a3") %>%
-    bcdata::filter(INTERSECTS(in_aoi)) %>%
-    collect() %>%
-    {if(nrow(.) > 0) st_intersection(., in_aoi) else .}
-
-
-  st_write(tem, file.path(out_path, "tem.gpkg"), delete_dsn = TRUE,
-           delete_layer = TRUE)
+  # st_write(tem, file.path(out_path, "tem.gpkg"), delete_dsn = TRUE,
+  #          delete_layer = TRUE)
 
 
   # 6) Water (Lakes, Rivers, Wetlands)
@@ -232,8 +200,8 @@ create_base_vectors <- function(in_aoi, out_path = "00_raw_inputs/vector"){
                      "f7dac054-efbf-402f-ab62-6fc4b32a619e", # rivers
                      "93b413d8-1840-4770-9629-641d74bd1cc6") # wetlands
 
-  cl <- parallel::makeCluster(parallel::detectCores())
-  doParallel::registerDoParallel(cl)
+  # cl <- parallel::makeCluster(parallel::detectCores())
+  # doParallel::registerDoParallel(cl)
 
   waterbodies <- foreach(i = water_records, .combine = rbind,
                          .packages = c("tidyverse", "bcdata", "sf")) %dopar%
@@ -243,7 +211,7 @@ create_base_vectors <- function(in_aoi, out_path = "00_raw_inputs/vector"){
         collect() %>%
         {if(nrow(.) > 0) st_intersection(., in_aoi) else NULL}
     }
-  parallel::stopCluster(cl)
+  # parallel::stopCluster(cl)
 
   # filter a subset of columns
   waterbodies_sf <- waterbodies %>%
@@ -301,7 +269,7 @@ create_base_vectors <- function(in_aoi, out_path = "00_raw_inputs/vector"){
                     "22c7cb44-1463-48f7-8e47-88857f207702")
 
   cl <- parallel::makeCluster(parallel::detectCores())
-  doParallel::registerDoParallel(cl)
+  # doParallel::registerDoParallel(cl)
 
   fires <- foreach(i = fire_records, .combine = rbind,
                    .packages = c("tidyverse", "bcdata", "sf")) %dopar%
@@ -315,7 +283,7 @@ create_base_vectors <- function(in_aoi, out_path = "00_raw_inputs/vector"){
             dplyr::filter(as.numeric(format(Sys.time(), "%Y")) - FIRE_YEAR <= 20)}
     }
 
-  parallel::stopCluster(cl)
+  # parallel::stopCluster(cl)
 
   st_write(fires, file.path(out_path, "fire.gpkg"), delete_dsn = TRUE,
            delete_layer = TRUE)
@@ -359,4 +327,49 @@ create_base_vectors <- function(in_aoi, out_path = "00_raw_inputs/vector"){
            delete_layer = TRUE)
 
 
-  }
+}
+
+
+
+
+# ## Overview
+#
+# This script downloads the relevant spatial data for BEC zones, VRI, TEM, waterbodies and the road network used in stage 1 of PEM processing. Data is downloaded directly from the [BC Data Catalogue](https://catalogue.data.gov.bc.ca/dataset?download_audience=Public) using the [bcdata](https://github.com/bcgov/bcdata) package.
+#
+# ### Datasets to download
+#
+# * BEC - [Biogeoclimatic Ecosystems Classification](https://catalogue.data.gov.bc.ca/dataset/bec-map) are used to define and/or select specific "subzones" within a defined study area. Note this can be also pointed to different file where more updated version is available.
+#
+# * Vegetation Resource Inventory (VRI) - This layer includes a variety of vegetation measure, including cutblock age, TEM data and ..... Detailed data standards can be found [here](https://www2.gov.bc.ca/gov/content/industry/forestry/managing-our-forest-resources/forest-inventory/data-management-and-access/vri-data-standards).
+#
+# * Freshwater Atlas -  The atlas is separated into the different types of waterbodies (lakes, rivers, wetlands, streams, man made, etc.), which requires a seperate download per type. Alternatively a single combined layer can be downloaded by is limited to linear data type. A parameter within the function can be set to "polygon" (the default option) or "linear".
+#
+# * Road network THIS STILL NEEEDS WORK
+#
+# * Road network - In previous works, the [raw road network](https://catalogue.data.gov.bc.ca/dataset/digital-road-atlas-dra-master-partially-attributed-roads) was found to be too detailed. The raw road network is filtered to only keep named roads, and then the up to date [Forest Service Road (FSR) layer](https://catalogue.data.gov.bc.ca/dataset/forest-tenure-road-segment-lines) is downloaded and merged with the filtered road network to produce a really good representation of where roads are actually located on the landscape.
+#
+# * Fire - Fire and fire intensity are used used to assist in identifying areas where a high cost is applied to reduce sampling. This includes:
+# -[current](https://catalogue.data.gov.bc.ca/dataset/fire-perimeters-current)
+# -[historical](https://catalogue.data.gov.bc.ca/dataset/fire-perimeters-historical) polygons.
+# -[severity](https://catalogue.data.gov.bc.ca/dataset/c58a54e5-76b7-4921-94a7-b5998484e697).
+#
+# * Accessing the [consolidated cutblock layer](https://catalogue.data.gov.bc.ca/dataset/harvested-areas-of-bc-consolidated-cutblocks-) using the bcdata package. The FTen layer is also used to identify very recent cutblocks ()
+#
+# Note the detailed
+# Tenure (private ownership) 	WHSE_CADASTRE.PMBC_PARCEL_FABRIC_POLY_FA_SVW	BCGW
+# Detailed roads layers. In some cases regionally specific roads are available through other collaborative projects (cummulative effects). These are not firectly available through the BCGW.
+
+
+# inputs: aoi_snapped (shapefile)
+# output filenames:
+
+
+
+
+
+# library(sf)
+# library(bcdata)
+# library(dplyr)
+# library(foreach)
+
+
