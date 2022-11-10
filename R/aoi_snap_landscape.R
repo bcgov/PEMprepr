d6f2ee746e05f27e3a12f6e03c0d6e044a03e234
Colin Chisholm <colin.chisholm@unbc.ca>
2022-11-10 00:49:08 +0000

    tweaks

--- R/aoi_snap_landscape.R
+++ R/aoi_snap_landscape.R
@@ -1,9 +1,11 @@
+
+
 # creates a aoi template at a larger scale for use in landscape covariates.
 # set to a watershed boundary
 
 #library(sf)
-library(bcdata)
-import(magrittr)
+# library(bcdata)
+# import(magrittr)
 
 #library(dplyr)
 #library(foreach)
@@ -13,11 +15,27 @@ import(magrittr)
 #out_path = shape_raw_dir <- file.path('temp')
 
 
+#' AOI Snap to landscape
+#'
+#' Creates a watershed based area of interest.  Does this by querying _BCData's_
+#' _Freshwater Atlas_
+#'
+#' @param in_aoi
+#'
+#' @return sf polygon
+#'
+#' @import magrittr
+#' @export
+#'
+#' @examples
+#' aoi_snap_landscape(aoi) ### add more
+#'
+
 aoi_snap_landscape <- function(in_aoi){
 
-  watershed <- bcdc_query_geodata("3ee497c4-57d7-47f8-b030-2e0c03f8462a") %>%
+  watershed <- bcdata::bcdc_query_geodata("3ee497c4-57d7-47f8-b030-2e0c03f8462a") %>%
     bcdata::filter(INTERSECTS(in_aoi)) %>%
-    collect()
+    bcdata::collect()
 
   box <- PEMprepr::aoi_snap(watershed, "extend")
 
