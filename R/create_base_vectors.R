eb5f188f49e1a3dc6859c0d7a95f6fe6bd2cd52a
Colin Chisholm <colin.chisholm@unbc.ca>
2022-11-15 17:51:49 +0000

    add withr for options call. plus tweaks

--- R/create_base_vectors.R
+++ R/create_base_vectors.R
@@ -4,6 +4,7 @@
 ## - options call ... change to global env.
 ## - review look for water ... overwriting issue?
 ## ... water is as far as I got.
+## sf::st_write syntax update... replace delete_dsn with append = FALSE
 ## make it BEC label?
 
 
@@ -86,18 +87,25 @@ create_base_vectors <- function(in_aoi, out_path = "00_raw_inputs/vector"){
 
   # Adjust max download size based on AOI
   ## PROBLEMATIC -- should not be done globally -----------------
-  options(bcdata.max_geom_pred_size = as.numeric(st_area(in_aoi)) + 10)
+  ## see: https://r-pkgs.org/code.html#sec-code-r-landscape section 7.6.1
+  withr::local_options(
+    options(bcdata.max_geom_pred_size = as.numeric(st_area(in_aoi)) + 10)
+  )
   #
   #
-  get_BEC(in_aoi, out_path) ##
+  get_BEC(in_aoi, out_path) ## works
+  # get_VRI(in_aoi, out_path) ## works
+  # get_harvest(in_aoi, out_path) ##works
 }
 
 
 
 
 
-## get_BEC ----------------------------
+## 1) Get_BEC ----------------------------
 get_BEC <- function(in_aoi, out_path) {
+  message("\rDownloading BEC layers")
+
 
   # # 1) BEC Biogeographical linework
   bec <- bcdc_query_geodata("f358a53b-ffde-4830-a325-a5a03ff672c3") %>%
@@ -105,41 +113,45 @@ get_BEC <- function(in_aoi, out_path) {
     collect() %>%
     {if(nrow(.) > 0) st_intersection(., in_aoi) else .}
 
-  st_write(bec, file.path(out_path, "bec.gpkg"), delete_dsn = TRUE,
-           delete_layer = TRUE)
+  st_write(bec, file.path(out_path, "bec.gpkg"), append = FALSE)
 
 
 }
 
-  # # 2) Download VRI
-  # vri <- bcdc_query_geodata("2ebb35d8-c82f-4a17-9c96-612ac3532d55") %>%
-  #   bcdata::filter(INTERSECTS(in_aoi)) %>%
-  #   bcdata::select(c("BCLCS_LEVEL_2","BCLCS_LEVEL_4","PROJ_AGE_CLASS_CD_1", "SPECIES_CD_1")) %>% # Treed sites
-  #   collect() %>%
-  #   {if(nrow(.) > 0) st_intersection(., in_aoi) else .}
-  #
-  # st_write(vri, file.path(out_path, "vri.gpkg"), delete_dsn = TRUE,
-  #          delete_layer = TRUE)
-  #
-  #
-  # # post process VRI data into classes:
-  # # Depending on the study area we want to focus on sampling in older areas - class 4 (60-80) or 5 (80 + )
-  # # To do this we will define 2 vri age classess to exclude from the sampling area
-  # # class 1 and 2 (0 - 40 yrs)
-  # # class 1-3 (0 - 60 years)
-  #
-  # #  vri <- st_read(file.path(out_path, "vri.gpkg")) %>%
-  # #    dplyr::select(c("BCLCS_LEVEL_2","BCLCS_LEVEL_4","PROJ_AGE_CLASS_CD_1", "SPECIES_CD_1"))
-  #
-  #     vri_class2 <- vri %>%
-  #       dplyr::mutate(age_class = as.numeric(PROJ_AGE_CLASS_CD_1)) %>%
-  #       dplyr::filter(age_class < 3)
-  #     st_write(vri_class2, file.path(out_path, "vri_class1_2.gpkg"))
-  #
-  #     vri_class3 <- vri %>%
-  #       dplyr::mutate(age_class = as.numeric(PROJ_AGE_CLASS_CD_1)) %>%
-  #       dplyr::filter(age_class == 3)
-  #     st_write(vri_class3, file.path(out_path, "vri_class3.gpkg"))
+## 2) Download VRI -----------------------
+get_VRI <- function(in_aoi, out_path) {
+  message("\rDownloading VRI layers")
+
+
+  vri <- bcdc_query_geodata("2ebb35d8-c82f-4a17-9c96-612ac3532d55") %>%
+    bcdata::filter(INTERSECTS(in_aoi)) %>%
+    bcdata::select(c("BCLCS_LEVEL_2","BCLCS_LEVEL_4","PROJ_AGE_CLASS_CD_1", "SPECIES_CD_1")) %>% # Treed sites
+    collect() %>%
+    {if(nrow(.) > 0) st_intersection(., in_aoi) else .}
+
+  st_write(vri, file.path(out_path, "vri.gpkg"), append = FALSE)
+
+
+  # post process VRI data into classes:
+  # Depending on the study area we want to focus on sampling in older areas - class 4 (60-80) or 5 (80 + )
+  # To do this we will define 2 vri age classess to exclude from the sampling area
+  # class 1 and 2 (0 - 40 yrs)
+  # class 1-3 (0 - 60 years)
+
+   vri <- st_read(file.path(out_path, "vri.gpkg")) %>%
+     dplyr::select(c("BCLCS_LEVEL_2","BCLCS_LEVEL_4","PROJ_AGE_CLASS_CD_1", "SPECIES_CD_1"))
+
+      vri_class2 <- vri %>%
+        dplyr::mutate(age_class = as.numeric(PROJ_AGE_CLASS_CD_1)) %>%
+        dplyr::filter(age_class < 3)
+      st_write(vri_class2, file.path(out_path, "vri_class1_2.gpkg"), append = FALSE)
+
+      vri_class3 <- vri %>%
+        dplyr::mutate(age_class = as.numeric(PROJ_AGE_CLASS_CD_1)) %>%
+        dplyr::filter(age_class == 3)
+      st_write(vri_class3, file.path(out_path, "vri_class3.gpkg"), append = FALSE)
+
+
   #
   #
   # ######### this section is currently in test phase (related to removal fo deciduous areas in Date Creek after these were found to be highly selected and challanging to sample)
@@ -149,50 +161,51 @@ get_BEC <- function(in_aoi, out_path) {
   # # STILL TO DO - for areas with deciduous leading (AT, EP) Aspen and paper burch these should be seperated using the code "SPECIES_CD_1" == AT|EP.
   # # ie important in Date Creek
   #
-  # vri_decid <- vri %>%
-  #   dplyr::filter(SPECIES_CD_1 %in% c("AT","EP")) # note might need to adjust for some areas of interest
-  #
-  # st_write(vri_decid, file.path(out_path, "vri_decid.gpkg"))
-  #
-  # ######### end of test section
-  #
-  # # 3) Download recent cutblocks (within last 20 years)
-  # message("\rDownloading cutblock layers")
-  #
-  # # Uses date filter which filters cutblock ages less than 20 years, or 7305 days
-  # cutblocks <- bcdc_query_geodata("b1b647a6-f271-42e0-9cd0-89ec24bce9f7") %>%
-  #   bcdata::filter(INTERSECTS(in_aoi)) %>%
-  #   collect()
-  #
-  # cutblocks <- cutblocks %>%
-  #   {if(nrow(.) > 0) st_intersection(., in_aoi) else .} %>%
-  #   dplyr::filter(as.numeric(format(Sys.time(), "%Y")) - HARVEST_YEAR <= 20)
-  #
-  # st_write(cutblocks, file.path(out_path, "cutblocks.gpkg"), delete_dsn = TRUE,
-  #          delete_layer = TRUE)
-  #
-  # # 4) ften  - Download latest harvest layer
-  #
-  # ften <- bcdc_query_geodata("cff7b8f7-6897-444f-8c53-4bb93c7e9f8b") %>%
-  #   bcdata::filter(INTERSECTS(in_aoi)) %>%
-  #   bcdata::select("HARVEST_AUTH_STATUS_CODE", "ISSUE_DATE", "CURRENT_EXPIRY_DATE_CALC", "LIFE_CYCLE_STATUS_CODE", "FILE_STATUS_CODE") %>%# Treed sites
-  #   collect()
-  #
-  #
-  # ften <- ften %>%
-  #   filter(ISSUE_DATE >2000) # might need to adjust this to dynamic
-  #
-  # ften <- ften %>%
-  #   dplyr::select("HARVEST_AUTH_STATUS_CODE", "ISSUE_DATE", "CURRENT_EXPIRY_DATE_CALC",
-  #                 "LIFE_CYCLE_STATUS_CODE", "FILE_STATUS_CODE","FEATURE_AREA")
-  #
-  # st_write(ften, file.path(out_path, "ften.gpkg"), delete_dsn = TRUE,
-  #          delete_layer = TRUE)
-  #
-  # cutblocks_ften <- bind_rows(cutblocks, ften)
-  # cutblocks_ften <- bind_rows( ften, cutblocks)
-  # st_write(cutblocks_ften, file.path(out_path, "cutblocks_ften.gpkg"),delete_dsn = TRUE,
-  #          delete_layer = TRUE)
+  vri_decid <- vri %>%
+    dplyr::filter(SPECIES_CD_1 %in% c("AT","EP")) # note might need to adjust for some areas of interest
+
+  st_write(vri_decid, file.path(out_path, "vri_decid.gpkg") , append = FALSE)
+
+}
+
+
+## 3) Get harvest history --------------------------------
+get_harvest <- function(in_aoi, out_path) {
+  # 3) Download recent cutblocks (within last 20 years)
+  message("\rDownloading cutblock layers")
+
+  # Uses date filter which filters cutblock ages less than 20 years, or 7305 days
+  cutblocks <- bcdc_query_geodata("b1b647a6-f271-42e0-9cd0-89ec24bce9f7") %>%
+    bcdata::filter(INTERSECTS(in_aoi)) %>%
+    collect()
+
+  cutblocks <- cutblocks %>%
+    {if(nrow(.) > 0) st_intersection(., in_aoi) else .} %>%
+    dplyr::filter(as.numeric(format(Sys.time(), "%Y")) - HARVEST_YEAR <= 20)
+
+  st_write(cutblocks, file.path(out_path, "cutblocks.gpkg"), append = FALSE)
+
+  # 4) ften  - Download latest harvest layer
+
+  ften <- bcdc_query_geodata("cff7b8f7-6897-444f-8c53-4bb93c7e9f8b") %>%
+    bcdata::filter(INTERSECTS(in_aoi)) %>%
+    bcdata::select("HARVEST_AUTH_STATUS_CODE", "ISSUE_DATE", "CURRENT_EXPIRY_DATE_CALC", "LIFE_CYCLE_STATUS_CODE", "FILE_STATUS_CODE") %>%# Treed sites
+    collect()
+
+
+  ften <- ften %>%
+    filter(ISSUE_DATE >2000) # might need to adjust this to dynamic
+
+  ften <- ften %>%
+    dplyr::select("HARVEST_AUTH_STATUS_CODE", "ISSUE_DATE", "CURRENT_EXPIRY_DATE_CALC",
+                  "LIFE_CYCLE_STATUS_CODE", "FILE_STATUS_CODE","FEATURE_AREA")
+
+  st_write(ften, file.path(out_path, "ften.gpkg"), append = FALSE)
+
+  cutblocks_ften <- dplyr::bind_rows(cutblocks, ften)
+  cutblocks_ften <- dplyr::bind_rows( ften, cutblocks)
+  st_write(cutblocks_ften, file.path(out_path, "cutblocks_ften.gpkg"), append = FALSE)
+}
   #
   #
   ### WORKS TO HERE ----------------------------------------------
