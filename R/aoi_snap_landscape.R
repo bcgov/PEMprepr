a1ac50e0b34d467585f70e1ffb3f4d8b0d78a52b
Colin Chisholm <colin.chisholm@unbc.ca>
2022-11-15 16:46:26 +0000

    fixed error, added option to preview

--- R/aoi_snap_landscape.R
+++ R/aoi_snap_landscape.R
@@ -20,7 +20,8 @@
 #' Creates a watershed based area of interest.  Does this by querying _BCData's_
 #' _Freshwater Atlas_
 #'
-#' @param in_aoi
+#' @param in_aoi `sf` polygon as the Area of Interest. Needs to be a meters based projection (e.g. Albers, UTM)
+#' @param preview Boolean.  `TRUE` will provide a mapview preview of the resulting aoi, the input aoi, and the Freshwater Atlas Watersheds that intersect.
 #'
 #' @return sf polygon
 #'
@@ -31,15 +32,17 @@
 #' aoi_snap_landscape(aoi) ### add more
 #'
 
-aoi_snap_landscape <- function(in_aoi){
+aoi_snap_landscape <- function(in_aoi, preview = TRUE){
 
   watershed <- bcdata::bcdc_query_geodata("3ee497c4-57d7-47f8-b030-2e0c03f8462a") %>%
     bcdata::filter(INTERSECTS(in_aoi)) %>%
     bcdata::collect()
 
-  box <- PEMprepr::aoi_snap(watershed, "extend")
+  box <- PEMprepr::aoi_snap(watershed, "expand")
 
- # mapview::mapview(list(box, in_aoi, watershed))
+  if (preview == TRUE) {
+      print(mapview::mapview(list(box, in_aoi, watershed)))
+  }
 
   return(box)
 
