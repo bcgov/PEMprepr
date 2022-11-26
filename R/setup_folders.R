b34c3507a88432c4d5a3361c3e31b03ca72586fc
gcperk <genevieve.c.perkins@gmail.com>
2022-11-25 17:33:24 -0800

    added newfolders and made unique folder names. Added absolute file path to output for SAGA use in later scripts

--- R/setup_folders.R
+++ R/setup_folders.R
@@ -1,6 +1,6 @@
-#' Setup Folder Structure
+#' Setup PEM folder structure
 #'
-#' Sets up the default folder structure
+#' Sets up the default folder structure for all analysis and modelling
 #'
 #' Folder structure contains all the raw data, covariates generated, and
 #' subsequent models, and predicted map rasters. _Note_ if directories have
@@ -17,70 +17,81 @@
 #' list.dir
 
 
-setup_folders <- function(aoi_name, full_names = TRUE){
+setup_folders <- function(aoi_name){
 
   # Testing ---------------
-  # full_names <- TRUE
   # aoi_name <- "e:/workRspace/tmp/apple123/apple" ## full path example
-  # aoi_name <- "carrot"  ## relative example
+  #aoi_name <- "carrot"  ## relative example
   ## END TESTING ----------
-
-  ## convert to full path if not provided
-  if (full_names == TRUE) {
-    if (is_absolute_path(aoi_name) == FALSE) {
-      aoi_name <- paste(as.character(getwd()), aoi_name, sep = "/")
-    }
-  }
 
   #base directory
   AOI_dir <- file.path(paste0(aoi_name))
-  raw_dir <- file.path(AOI_dir, "00_raw_inputs")
-  derived_dir <- file.path(AOI_dir, "10_map_inputs")
 
-  # input and data processing directly
+  # generate folders
+  raw_dir_00 <- file.path(AOI_dir, "00_raw_inputs")
+  dem_dir_00 <- file.path(raw_dir_00, "20_dem")
+  shape_dir_0010 <- file.path(raw_dir_00, "10_vector")
 
-  shape_dir <- file.path(AOI_dir,               "00_raw_inputs", "vector")
-  shape_raw_dir <- file.path(AOI_dir,           "00_raw_inputs", "vector", "raw")
-  dem_dir <- file.path(AOI_dir,                 "00_raw_inputs", "dem")
-  lidar_dir <- file.path(AOI_dir,               "00_raw_inputs", "dem", "lidar")
-  trim_dir <- file.path(AOI_dir,                "00_raw_inputs", "dem", "trim")
-  sat_dir  <- file.path(AOI_dir,                "00_raw_inputs", "satelite")
-  CHM_dir  <- file.path(AOI_dir,                "00_raw_inputs", "chm")
+  lidar_dir_00 <- file.path(dem_dir_00, "lidar")
+  trim_dir_00 <- file.path(dem_dir_00 , "trim")
 
-  cov_dir <- file.path(AOI_dir,                 "10_clean_inputs", "covariates")
+  chm_dir_00  <- file.path(raw_dir_00, "40_chm")
+  sat_dir_00  <- file.path(raw_dir_00, "30_satelitte")
 
+
+  cov_dir_10 <- file.path(AOI_dir, "10_clean_inputs")
+  shape_dir_1010 <- file.path(cov_dir_10, "10_vector")
+  cov_dir_1020 <- file.path(cov_dir_10, "20_covariates")
+  training_data_1030 <- file.path(cov_dir_10, "30_trainingdata")
+  # note the raster size folders are generated in other script
+
+  sample_dir_20 <- file.path(AOI_dir, "20_sample_plan")
   # sample filepaths
-  out_path <- file.path(AOI_dir,                "20_sample_design", "stage1_studydesign")
-  sampling_raw_folder <- file.path(out_path,    "input_raster")
-  clhs_outpath <- file.path(out_path,           "clhs_sample_plans")
+  out_path_2010 <- file.path(sample_dir_20 , "10_standard_sample")
+  sampling_input_2010 <- file.path(out_path_2010, "10_input_raster")
+  sampling_input_landscape <- file.path(out_path_2010, "10_input_raster", "landscape_covariates")
+  sampling_input_exclusion <- file.path(out_path_2010, "10_input_raster", "exclusion")
+  sampling_input_review <- file.path(out_path_2010, "10_input_raster", "review")
 
 
-  training_data <- file.path(AOI_dir,           "10_clean_inputs", "trainingdata")
-  training_data_vector <- file.path(AOI_dir,    "10_clean_inputs", "vector")
-  training_data_clean <- file.path(AOI_dir,     "10_clean_inputs", "trainingdata", "clean")
-
-  # model building folders
-  model_dir <- file.path(AOI_dir,               "30_maps_analysis")
-  model_data <- file.path(AOI_dir,              "30_maps_analysis", "models")
-  model_f <- file.path(AOI_dir,                 "30_maps_analysis", "models", "forest")
+  samplingplan_201020 <- file.path(out_path_2010, "20_sampleplan_draft")
+  samplingplan_clhs <- file.path(samplingplan_201020, "clhs")
+  samplingplan_vrp <- file.path(samplingplan_201020, "vrp")
+  samplingplan_review <- file.path(samplingplan_201020,  "review")
 
 
-  ## add error control
+  sample_dir_001030 <- file.path( out_path_2010, "30_sampleplan_final")
+  sampleplan_final_transect <- file.path(sample_dir_001030 , "transect")
+  sampleplan_final_maps<- file.path(sample_dir_001030 , "maps")
+
+
+  trainpts_201040 <- file.path( out_path_2010, "40_transect_data")
+  trainpts_transect <- file.path(trainpts_201040  , "raw_field_data")
+  trainpts_maps <- file.path(trainpts_201040 , "clean_field_data")
+
+  remoteplan_201020 <- file.path(sample_dir_20, "20_remote_sample")
+
+  trans_review2030 <- file.path(sample_dir_20, "30_transect_review")
+
+
+  sample_dir_0030 <- file.path(AOI_dir, "30_model")
 
 
   ## set up folders if not already exist
   ## also used to create list of default directories
-  folder_set_up <- data.frame(AOI_dir, raw_dir,shape_raw_dir, derived_dir,
-                              cov_dir, shape_dir, dem_dir,lidar_dir, trim_dir,
-                              out_path, sampling_raw_folder, clhs_outpath,
-                              training_data, training_data_clean, training_data_vector,
-                              model_dir, model_data, model_f,
-                              sat_dir, CHM_dir)
+  folder_set_up <- data.frame(AOI_dir, raw_dir_00,  dem_dir_00 , shape_dir_0010 ,
+                              lidar_dir_00, trim_dir_00, chm_dir_00, sat_dir_00,
+                              cov_dir_10,shape_dir_1010, cov_dir_1020, training_data_1030,
+                              sample_dir_20 , out_path_2010, sampling_input_2010, sampling_input_landscape,
+                              sampling_input_exclusion, sampling_input_review,
+                              samplingplan_201020 ,  samplingplan_clhs,samplingplan_vrp,  samplingplan_review,
+                              sample_dir_0030,  sampleplan_final_transect, sampleplan_final_maps,
+                              trainpts_201040 ,trainpts_transect, trainpts_maps,
+                              remoteplan_201020 ,trans_review2030)
 
+  # generate absolute paths
+  folder_set_up <- rbind(folder_set_up, R.utils::getAbsolutePath(folder_set_up))
 
-
-
-  ### Compare if dirs exist ... or if there are additionsal dirs
   folder_id <- as.list(folder_set_up)
 
   # either way return the folder list.
@@ -92,32 +103,19 @@ setup_folders <- function(aoi_name, full_names = TRUE){
 
   tf$exists <- dir.exists(tf$path)
 
+  if(sum(tf$exists) < nrow(tf)) {
 
-if(sum(tf$exists) < nrow(tf)) {
     print("creating folders")
+
     for(fold in folder_set_up){
-
       ifelse(!dir.exists(fold), dir.create(fold, recursive = TRUE), FALSE)
-
-
-
     }
-  print("Folder structure created!")
-
+    print("folder structure created")
   } else {
-
     print(paste("The", as.character(aoi_name), "folder already exists - returning folder names."))
 
   }
 
-
   return(folder_id)
 
-
 }
-#
-#
-# is_absolute_path <- function(path) {
-#   grepl("^(/|[A-Za-z]:|\\\\|~)", path)
-# }
-#
