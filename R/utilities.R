5900fce6b18586f93f02a8a6d72ae39945ef5cec
Tristan Goodbody <60200460+tgoodbody@users.noreply.github.com>
2022-12-15 12:54:20 -0800

    Update utilities.R
    add utility functions:
    
    `recursive_layers_call()` - take in user desired layers and check to see if any require recursive inputs (e.g. some layers require `sinksfilled` as input but it may not be specified by the user in their `layers` call.)
    
    `parallel_create_covariates()` - perform `create_covaraites()` in parallel. Intended for tiled processing.
    
    `mosaic_metrics()` - mosaic all tiled covariates from `parallel_create_covaraites()`.
    
    `read_crop()` - internal to `mosaic_covaraites()`. Read in overlapping tiles and crop to un buffered extent. This removes edge effects for covariates that are OK to run in parallel.
    
    `covariate_file_names()` - moved from `create_covaraites()` for simplicity. Is called in `create_covariates()` to assign file paths to module outputs.

--- R/utilities.R
+++ R/utilities.R
@@ -8,3 +8,251 @@
 is_absolute_path <- function(path) {
   grepl("^(/|[A-Za-z]:|\\\\|~)", path)
 }
+
+
+####--- recursive calling of SAGA modules & warn if modules generate artifacts ---####
+#--- moddir and artifacts are internal data in the data/ subdirectory ---#
+recursive_layers_call <- function(layers, moddir = PEMprepr::moddir, artifact = PEMprepr::artifacts){
+
+  #--- which modules are being called ---#
+
+  moddirs <- moddir %>%
+    dplyr::filter(module %in% layers)
+
+  mods <- moddirs$module
+
+  #--- determine recursive inputs of desired modules ---#
+
+  while(any(moddirs[,2:3] != "")){
+
+    p1 <- unique(moddirs$par1)
+
+    p1 <- p1[!p1 %in% mods]
+
+    p2 <- unique(moddirs$par2)
+
+    p2 <- p2[!p2 %in% mods]
+
+    v1 <- c(p1,p2)
+
+    v1 <- v1[v1 != ""]
+
+    moddirs <- moddir %>%
+      dplyr::filter(module %in% v1)
+
+    mods <- c(mods,moddirs$module)
+  }
+
+  #--- ordered vector of modules (and their recursive inputs) to call ---#
+
+  layers_to_call <- mods[order(match(mods, moddir$module))]
+
+
+  #--- if artifact is provided report the modules that produce artifacts if they are requested ---#
+
+  if(!is.null(artifact)){
+
+    artifactdirs <- artifact %>%
+      dplyr::filter(artifacts == TRUE) %>%
+      dplyr::select(metric)
+
+    artifact_layers <- layers_to_call[layers_to_call %in% artifactdirs$metric]
+
+    if(length(artifact_layers) != 0){
+
+      warning(paste0(artifact_layers, collapse = ", ")," -- produce artifacts during tiled processing.")
+
+    }
+
+  }
+
+  #--- modules needed to be called based on inputs ---#
+
+  return(layers_to_call)
+
+}
+####--- parallel processing of tiles to generate SAGA metrics ---####
+
+parallel_create_covariates <- function(files,
+                                       layers,
+                                       cores,
+                                       output,
+                                       SAGApath = "",
+                                       fx = PEMprepr::create_covariates){
+
+  pb <- utils::txtProgressBar(max = length(files)*length(layers), style = 3)
+  progress <- function(n) utils::setTxtProgressBar(pb, n)
+  opts <- list(progress = progress)
+
+  cores = cores
+  cl <- snow::makeCluster(spec = cores, type = "SOCK")
+  doSNOW::registerDoSNOW(cl)
+
+  loop <- foreach::foreach(f = layers, .packages = c("terra","PEMprepr"), .options.snow = opts) %:%
+
+    foreach::foreach(i = files) %dopar% {
+
+      fx(dtm = i, SAGApath = SAGApath, output = output, layers = f)
+
+    }
+
+  close(pb)
+
+  #--- End parallel ---#
+  snow::stopCluster(cl)
+
+}
+
+
+####--- mosaic SAGA tiles into seamless rasters ---####
+mosaic_metrics <- function(dir, output, poly, tmp){
+
+  #--- make output and tmp if it doesnt already exist ---#
+
+  dir.create(output, recursive = TRUE)
+  dir.create(tmp, recursive = TRUE)
+
+  #--- read in poly vector ---#
+  poly <- terra::vect(poly)
+
+  #--- list file directory rasters to mosaic ---#
+  f <- list.files(dir, pattern = ".sdat$")
+
+  #--- filter unique metric names in files ---#
+  l <- unique(unlist(stringr::str_split(f,pattern = "\\s*([[:digit:]]+)_")))
+
+  l1 <- l[!l %in% "tile_"]
+
+  #--- mosaic metrics ---#
+  for(i in 1:length(l1)){
+
+    f <- list.files(dir, pattern = paste0(l1[i],"$"), full.names = TRUE)
+
+    #--- define output mosaic name ---#
+    out <- paste0(output,tools::file_path_sans_ext(basename(l1[i])),".tif")
+
+    #--- write mosaiced outputs ---#
+
+    if(!file.exists(out)){
+
+      #--- read and crop overlapping tiles to polygon extent ---#
+
+      walk(.x = f, .f = read_crop, poly = poly, tmp = tmp)
+
+      tmpf <- list.files(tmp, full.names = TRUE, pattern = ".sdat$")
+
+      v <- terra::vrt(tmpf)
+
+      message(paste0("mosaicing -- ",l1[i]))
+
+      terra::writeRaster(v, paste0(output,tools::file_path_sans_ext(basename(l1[i])),".tif"))
+
+      #--- delete tmp directory files ---#
+      do.call(file.remove, list(list.files(tmp, full.names = TRUE)))
+
+    } else {
+
+      message("exists")
+    }
+  }
+
+  #--- delete temp directory ---#
+
+  unlink(tmp, recursive = TRUE)
+
+}
+
+####--- read in overlapping tiles and crop using tile extents ---####
+read_crop <- function(f, poly, tmp){
+
+  #--- extract tile index to match with index in polygons - works up to 1000 tiles ---#
+  index <- stringr::str_extract(f, pattern = "_([1-9]|[1-9][0-9]|[1-9][0-9][0-9]|1000)_") %>%
+    stringr::str_replace_all(., pattern = "_", replacement = "") %>%
+    as.numeric()
+
+  #--- write rasters to tmp folder ---#
+  terra::writeRaster(terra::rast(f) %>% terra::crop(y = poly[index,]), paste0(tmp,basename(f)), overwrite = TRUE)
+
+}
+
+####--- create covaraite file names ---####
+
+covariate_file_names <- function(outputdir, nm){
+
+  sinkroute <- paste0(outputdir,"/sinkroute/",nm,"_sinkroute.sgrd")
+  sinksfilled <- paste0(outputdir,"/sinksfilled/",nm,"_sinksfilled.sgrd")
+  dem_preproc <- paste0(outputdir,"/dem_preproc/",nm,"_dem_preproc.sgrd")
+  slope <- paste0(outputdir,"/slope_aspect_curve/",nm,"_slope.sgrd")
+  aspect <- paste0(outputdir,"/slope_aspect_curve/",nm,"_aspect.sgrd")
+  gencurve <- paste0(outputdir,"/slope_aspect_curve/",nm,"_gencurve.sgrd")
+  totcurve <- paste0(outputdir,"/slope_aspect_curve/",nm,"_totcurve.sgrd")
+  tcatchment <- paste0(outputdir,"/tcatchment/",nm,"_tcatchment.sgrd")
+  tca <- paste0(outputdir,"/tca/",nm,"_tca1.sgrd")
+  flowlength4 <- paste0(outputdir,"/tca/",nm,"_flowlength1.sgrd") ## part of tca
+  scatchment <- paste0(outputdir,"/scatchment/",nm,"_scatchment.sgrd")
+  twi <- paste0(outputdir,"/twi/",nm,"_twi.sgrd")
+  channelsnetwork <- paste0(outputdir,"/channelsnetwork/",nm,"_cnetwork.sgrd")
+  hdistance <-  paste0(outputdir,"/overlandflow/",nm,"_hdist.sgrd")
+  vdistance  <- paste0(outputdir,"/overlandflow/",nm,"_vdist.sgrd")
+  hdistancenob <-  paste0(outputdir,"/overlandflow2/",nm,"_hdistnob.sgrd")
+  vdistancenob  <- paste0(outputdir,"/overlandflow2/",nm,"_vdistnob.sgrd")
+  MRVBF <- paste0(outputdir,"/multiresflatness/",nm,"_mrvbf.sgrd")
+  MRRTF <- paste0(outputdir,"/multiresflatness/",nm,"_mrrtf.sgrd")
+  MRVBF2 <- paste0(outputdir,"/multiresflatness2/",nm,"_mrvbf2.sgrd")
+  MRRTF2 <- paste0(outputdir,"/multiresflatness2/",nm,"_mrrtf2.sgrd")
+  tri <- paste0(outputdir,"/tri/",nm,"_tri.sgrd")
+  convergence <- paste0(outputdir,"/convergence/",nm,"_convergence.sgrd")
+  opos <- paste0(outputdir,"/openness/",nm,"_open_pos.sgrd")
+  oneg <- paste0(outputdir,"/openness/",nm,"_open_neg.sgrd")
+  dAH <- paste0(outputdir,"/dah/",nm,"_dah.sgrd")
+  tpi <- paste0(outputdir,"/tpi/",nm,"_tpi.sgrd")
+  val_depth <- paste0(outputdir,"/ridgevalley/",nm,"_val_depth.sgrd")
+  ridgelevel <- paste0(outputdir,"/ridgevalley/",nm,"_rid_level.sgrd")
+  mrncatchment <- paste0(outputdir,"/mrn/",nm,"_mrn_area.sgrd")
+  mrnmaxheight <- paste0(outputdir,"/mrn/",nm,"_mrn_mheight.sgrd")
+  mrn <- paste0(outputdir,"/mrn/",nm,"_mrn.sgrd")
+  flowaccumft <- paste0(outputdir,"/flowaccumulation/",nm,"_flow_accum_ft.sgrd")
+  meanovcatch <- paste0(outputdir,"/flowaccumulation/",nm,"_meanovcatch.sgrd")
+  accummaterial <- paste0(outputdir,"/flowaccumulation/",nm,"_accummaterial.sgrd")
+  slopelength <- paste0(outputdir,"/slopelength/",nm,"_slength.sgrd")
+  flowaccump <- paste0(outputdir,"/flowaccumulation2/",nm,"_flow_accum_p.sgrd")
+  flowaccum <- paste0(outputdir,"/flowpathlength3/",nm,"_flow_accum.sgrd")
+  flowaccumtd <- paste0(outputdir,"/flowaccumulation3/",nm,"_flow_accum_td.sgrd")
+  meanovcatchTD <- paste0(outputdir,"/flowaccumulation3/",nm,"_meanovcatchTD.sgrd")
+  accummaterialTD <- paste0(outputdir,"/flowaccumulation3/",nm,"_accummaterialTD.sgrd")
+  flowpathlenTD <- paste0(outputdir,"/flowaccumulation3/",nm,"_flowpathlenTD.sgrd")
+  flowpathlength <- paste0(outputdir,"/flowpathlength/",nm,"_max_fp_l.sgrd")
+  flowpathlength1 <- paste0(outputdir,"/flowpathlength2/",nm,"_max_fp_l1.sgrd")
+  #FloOwAccum <- paste0(outputdir,"/slope_lts_fa/",nm,"_slope_lts_fa.sgrd") isnt called in create_covariates()
+  lsfactor <- paste0(outputdir,"/lsfactor/",nm,"_ls_factor.sgrd")
+  DirInsol <- paste0(outputdir,"/solarrad/",nm,"_direinso.sgrd")
+  DifInsol <- paste0(outputdir,"/solarrad/",nm,"_diffinso.sgrd")
+  convexity <- paste0(outputdir,"/convexity/",nm,"_convexity.sgrd")
+  vertdistance <- paste0(outputdir,"/vertdistance/",nm,"_vert_dis.sgrd")
+  tci_low <- paste0(outputdir,"/tci_low/",nm,"_tci_low.sgrd")
+  catchmentarea <- paste0(outputdir,"/swi/",nm,"_swi_area.sgrd")
+  catchmentslope <- paste0(outputdir,"/swi/",nm,"_swi_slope.sgrd")
+  modcatchmentarea <- paste0(outputdir,"/swi/",nm,"_swi_area_mod.sgrd")
+  topowetindex <- paste0(outputdir,"/swi/",nm,"_swi_twi.sgrd")
+  windexp <- paste0(outputdir,"/windexp",nm,"_wind_exp_index.sgrd")
+  texture <- paste0(outputdir,"/texture/",nm,"_texture.sgrd")
+  protection <- paste0(outputdir,"/protection/",nm,"_protection.sgrd")
+  vrm <- paste0(outputdir,"/vrm/",nm,"_vrm.sgrd")
+  mbi <- paste0(outputdir,"/mbi/",nm,"_mbi.sgrd")
+  tpi <- paste0(outputdir,"/mscale_tpi/",nm,"_mscale_tpi.sgrd")
+  slopeheight <- paste0(outputdir,"/relposition/",nm,"_slope_height.sgrd")
+  valleydepth <- paste0(outputdir,"/relposition/",nm,"_valleydepth.sgrd") #don't need this as created above?
+  normheight <- paste0(outputdir,"/relposition/",nm,"_norm_height.sgrd")
+  standheight <- paste0(outputdir,"/relposition/",nm,"_stand_height.sgrd")
+  msposition <- paste0(outputdir,"/relposition/",nm,"_ms_position.sgrd")
+  localcurve <- paste0(outputdir,"/slopecurvatures/",nm,"_local_curv.sgrd")
+  upslopecurve <- paste0(outputdir,"/slopecurvatures/",nm,"_upslope_curv.sgrd")
+  localupcurve <- paste0(outputdir,"/slopecurvatures/",nm,"_local_upslope_curv.sgrd")
+  downcurve <- paste0(outputdir,"/slopecurvatures/",nm,"_down_curv.sgrd")
+  localdowncurve <- paste0(outputdir,"/slopecurvatures/",nm,"_local_downslope_curv.sgrd")
+  steepestslope <- paste0(outputdir,"/steepestslope/",nm,"_steepest_slope.sgrd")
+  upslopearea <- paste0(outputdir,"/upslopearea/",nm,"_upslopearea.sgrd")
+
+  environment()
+
+}
