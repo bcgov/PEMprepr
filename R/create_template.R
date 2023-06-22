20458b5b476a2a8a8607c3085b9542d9473d62e4
gcperk <genevieve.c.perkins@gmail.com>
2023-06-22 16:45:14 -0700

    update create_template ()

--- R/create_template.R
+++ R/create_template.R
@@ -6,33 +6,26 @@
 #' This is an essential first step.  As subsequent co-variate layers will be generated at multiple resolutions (e.g. 5, 10, 25m^2) and then disaggregate'd back to the lowest resolution.
 #' Having the aoi set 100m break-points facilitates this.
 #'
-#' @param aoi is a sf or terra::vect object bounding box created expanded in aoi_snap function(e.g. polygon).  Should be a meter based projection
+#' @param aoi_bb is a sf or terra::vect object bounding box created expanded in aoi_snap function(e.g. polygon).  Should be a meter based projection
 #' @param res desired resolution of the raster (in meters)
+#' @param filename text name of the output file ie: template.tif
 #' @param outpath output path.  Note that the results will be placed in a subfolder labelled with the resolution.
-#' @return a terra raster.  _NOT CURRENTLY_ saving raster objects
-#' @keywords aoi, raster, template, crop, align
+#'
+#' @return a terra raster and saves in correct filepath
+#' @keywords aoi, raster, template
 #' @export
 #' @examples
-#'
-#' create_template(aoi, 10)
-
-## TESTING ----------
-## REMOVE  --
-# read in the aoi
-# res = 10
-# aoi <- terra::vect( "./temp_data/aoi.gpkg")
-# setupfolders("CanyonCreek2")
-
-# output = cov_dir
-## ------------------
+#' create_template(aoi_bb = file.path(fid$shape_dir_1010[1], "aoi_snapped.gpkg"), res = 10, outpath = fid$cov_dir_1020[2], filename = "template.tif")
 
 
-create_template <- function(aoi, res, outpath = fid$cov_dir_1020[2]){
+create_template <- function(aoi_bb = file.path(fid$shape_dir_1010[1], "aoi_snapped.gpkg"),
+                            res, outpath = fid$cov_dir_1020[2],
+                            filename = "template.tif"){
   if (class(aoi)[1] == "sf") {
-    aoi <- terra::vect(aoi)
+    aoi_bb  <- terra::vect(aoi)
   }
 
-  template <- terra::rast(aoi, resolution = res)
+  template <- terra::rast(aoi_bb , resolution = res)
   terra::values(template) <- 0
 
   outpath <- file.path(outpath, paste0(res, 'm'))
@@ -40,13 +33,10 @@ create_template <- function(aoi, res, outpath = fid$cov_dir_1020[2]){
 
 
   # write out
-  terra::writeRaster(template, file.path(outpath, "template21.tif"), overwrite = TRUE)
+  terra::writeRaster(template, file.path(outpath, filename), overwrite = TRUE)
 
   # print(paste("Template raster create and saved as:", file.path(outpath, "template.tif")))
   return(template)
-}
 
-
-
-## -----------------------------------
+  }
 
